f79d76ff09823cd8f37e7bed1419afc66a7beaca
Eva-Mykhno <154619097+Eva-Mykhno@users.noreply.github.com>
2025-03-26 11:43:05 +0200

    add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import StatusLabel, { Status } from './components/status-label';
+import { headers } from 'next/headers';
 
-export default function Home() {
+export default async function Home() {
+  console.log(Object.fromEntries((await headers()).entries()));
+
   return (
     <main>
-      <h1 className="text-xl">Home page</h1>
+      <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>{' '}
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>{' '}
